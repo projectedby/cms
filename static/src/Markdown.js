1209a79551a051cc03a1603ef624d8bb75638fac
novemberizing <novemberizing@gmail.com>
2023-11-29 15:13:45 +0900

    Update Programing Tip

--- static/src/Markdown.js
+++ static/src/Markdown.js
@@ -2,7 +2,10 @@ import showdown from "showdown";
 import yaml from "yaml";
 
 export default class Markdown {
-    static #converter = new showdown.Converter({metadata: true});
+    static #converter = new showdown.Converter({
+        metadata: true,
+        tables: true
+    });
 
     static parse(o) {
         const html = Markdown.#converter.makeHtml(o);
